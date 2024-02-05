efbba00ed167128f4c97a72af90a0eaaa0ff6b3d
Juanma <35576283+jmmolins87@users.noreply.github.com>
2024-02-05 16:09:52 +0100

    fix all components

---
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { SideMenu } from '../../interfaces/sideMenu.interface';
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.css']
+})
+export class SideMenuComponent {
+
+  public menuItems: SideMenu[] = [
+    { title: 'Contador', route: 'counter' },
+    { title: 'Usuario', route: 'user-info' },
+    { title: 'Mutaciones', route: 'properties' }
+  ]
+
+}
---
+++ src/app/signals/interfaces/sideMenu.interface.ts
@@ -0,0 +1,6 @@
+
+
+export interface SideMenu {
+    title: string;
+    route: string
+}
